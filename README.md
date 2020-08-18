ea38c72b7b01a357f9931453c8ea3ee690be1caf
Vicky <weiling.yu.vicky@gmail.com>
2020-08-18 13:56:39 -0700

    Mongoose part1

---
+++ README.md
@@ -0,0 +1,3 @@
+Coursera / Server-side / W2-Mongoose ODM
+-Define and construct Mongoose schemas and models.
+-Interact with MongoDB server using Mongoose from the Node application.
---
+++ index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log('Connected correctly to server');
+    let newDish = Dishes({
+        name: 'Uthappizza',
+        description: 'test'
+    });
+    newDish.save()
+        .then((dish) => {
+            console.log('find:', dish);
+            return Dishes.find({}).exec();
+        })
+        .then((dishes) => {
+            console.log('remove: ', dishes);
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
