58cf0073212770306b3fe9287e95f36648ac9911
Vicky <weiling.yu.vicky@gmail.com>
2020-08-18 14:02:29 -0700

    Mongoose ODM Part 2: Use create method and chain the promises

--- index.js
+++ index.js
@@ -6,23 +6,27 @@ const connect = mongoose.connect(url);
 
 connect.then((db) => {
     console.log('Connected correctly to server');
-    let newDish = Dishes({
+    // let newDish = Dishes({
+    //     name: 'Uthappizza',
+    //     description: 'test'
+    // });
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
+    })
+    // newDish.save()
+    .then((dish) => {
+        console.log('find:', dish);
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log('remove: ', dishes);
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-    newDish.save()
-        .then((dish) => {
-            console.log('find:', dish);
-            return Dishes.find({}).exec();
-        })
-        .then((dishes) => {
-            console.log('remove: ', dishes);
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 })
