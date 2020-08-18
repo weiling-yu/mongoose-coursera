86cf41c39e39c9edb7d0a597f3082c6f74eb6a3f
Vicky <weiling.yu.vicky@gmail.com>
2020-08-18 14:40:04 -0700

    Add sub-document. and create, update and save document

--- index.js
+++ index.js
@@ -6,21 +6,34 @@ const connect = mongoose.connect(url);
 
 connect.then((db) => {
     console.log('Connected correctly to server');
-    // let newDish = Dishes({
-    //     name: 'Uthappizza',
-    //     description: 'test'
-    // });
     Dishes.create({
         name: 'Uthappizza',
         description: 'test'
     })
-    // newDish.save()
     .then((dish) => {
-        console.log('find:', dish);
-        return Dishes.find({}).exec();
+        console.log(dish);
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
-    .then((dishes) => {
-        console.log('remove: ', dishes);
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
         return Dishes.remove({});
     })
     .then(() => {
--- models/dishes.js
+++ models/dishes.js
@@ -1,16 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    commment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true,
+    }
+}, {
+    timestamps: true
+});
+
 const dishSchema = new Schema ({
     name: {
         type: String,
-        require: true,
+        required: true,
         unique: true
     },
     description: {
         type: String,
-        require: true
-    }
+        required: true
+    },
+    comments: [ commentSchema ]
 },{
     timestamps: true
 });
